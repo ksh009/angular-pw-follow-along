181cdd92cfd03856726c883afc1b00b1a883f2cb
kyle <khughessean01@gmail.com>
2023-10-08 12:49:43 +0200

    methods and evt binding for all checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+    console.log("checbox ticked: ", this.includeLetters)
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+    console.log("checbox ticked: ", this.includeNumbers)
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+    console.log("checbox ticked: ", this.includeSymbols)
+  }
+
   onButtonClick() {
+    console.log("includeLetters", this.includeLetters)
+    console.log("includeNumbers", this.includeNumbers)
+    console.log("includeSymbols", this.includeSymbols)
+
     this.password = 'MY PASSWORD!!!';
-    console.log("Button clicked!!!")
   }
 
   getName() {

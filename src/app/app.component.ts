d4428eb9f517cc99f8a813509bf6936702d1e273
kyle <khughessean01@gmail.com>
2023-10-09 18:31:21 +0200

    resolve value error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,38 +3,48 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  length = 0;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
   password = '';
 
+  onChangeLength(event: Event) {
+    // console.log('Updated input event', value);
+    const parsedValue = parseInt((event.target as HTMLInputElement).value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
   onChangeUseLetters() {
     this.includeLetters = !this.includeLetters;
-    console.log("checbox ticked: ", this.includeLetters)
+    console.log('checbox ticked: ', this.includeLetters);
   }
 
   onChangeUseNumbers() {
     this.includeNumbers = !this.includeNumbers;
-    console.log("checbox ticked: ", this.includeNumbers)
+    console.log('checbox ticked: ', this.includeNumbers);
   }
 
   onChangeUseSymbols() {
     this.includeSymbols = !this.includeSymbols;
-    console.log("checbox ticked: ", this.includeSymbols)
+    console.log('checbox ticked: ', this.includeSymbols);
   }
 
   onButtonClick() {
-    console.log("includeLetters", this.includeLetters)
-    console.log("includeNumbers", this.includeNumbers)
-    console.log("includeSymbols", this.includeSymbols)
+    console.log('includeLetters', this.includeLetters);
+    console.log('includeNumbers', this.includeNumbers);
+    console.log('includeSymbols', this.includeSymbols);
 
     this.password = 'MY PASSWORD!!!';
   }
 
   getName() {
-    return "Luke Skywalker"
+    return 'Luke Skywalker';
   }
 }
